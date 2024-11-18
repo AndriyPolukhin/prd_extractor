6eeba4a81bfa33c2868f6ca5e8c2edb086c96842
Andriy Polukhin <andriy.polukhin@gmail.com>
2024-11-18 02:49:47 +0200

    added setting and updated links

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import { Container, Card, Button } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 
 const Hero = () => {
 	return (
@@ -10,12 +11,14 @@ const Hero = () => {
 						Please login or register in order to work with your pdf files.
 					</p>
 					<div className='d-flex'>
-						<Button variant='primary' href='/login' className='me-3'>
-							Sign In
-						</Button>
-						<Button variant='secondary' href='/register'>
-							Register
-						</Button>
+						<LinkContainer to='/login'>
+							<Button variant='primary' className='me-3'>
+								Sign In
+							</Button>
+						</LinkContainer>
+						<LinkContainer to='/register'>
+							<Button variant='secondary'>Register</Button>
+						</LinkContainer>
 					</div>
 				</Card>
 			</Container>
--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -4,6 +4,8 @@ export const allowedOrigins = [
 	'http://localhost:5000',
 	'http://127.0.0.1:3000',
 	'0.0.0.0/1',
+	'https://prd-extractor-client.onrender.com',
+	'https://prd-extractor-server.onrender.com',
 ]
 // fetch('http://localhost:3500')
 export default allowedOrigins
